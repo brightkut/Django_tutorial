298d3672dac8ee5af4718e1d5bcbee7f573c9a25
Bright <dsorn2@gmail.com>
2019-06-04 13:47:22 +0700

    implement api with create route and template already work

--- basicdjango/basicdjango/settings.py
+++ basicdjango/basicdjango/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'basicdjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['hello/template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- basicdjango/hello/views.py
+++ basicdjango/hello/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello</h1>")
+  template=loader.get_template('index.html')
+
+  #demo object like a database that send back to front 
+  human = {
+      'name': "bright",
+      'age': 22 ,
+      'food': ['water','soda','apple']
+
+  }
+  return HttpResponse(template.render(human,request))
 
